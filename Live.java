85922ada44420fffcf718dc4159fdd1161112362
Alex <alex97av@gmail.com>
2023-07-14 14:51:08 +0200

    aggiunta del ciclo con while

--- Live.java
+++ Live.java
@@ -2,32 +2,37 @@ import java.util.Scanner;
 public class Live {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
+        boolean run=true;
+        while(run){
 
-        int number;
+            int number;
 
-        number = console.nextInt();
+            number = console.nextInt();
 
-        switch (number) {
-            case 1:
-                System.out.println("Number = ONE");
-                break;
-            case 2:
-                System.out.println("Number = TWO");
-                break;
-            case 3:
-                System.out.println("Number = THREE");
-                break;
-            case 4:
-                System.out.println("Number = FOUR");
-                break;
-            case 5:
-                System.out.println("Number = FIVE");
-                break;
-            case 6:
-                System.out.println("Number = SIX");
-                break;
-            default:
-                System.out.println("Cannot give you the name");
+            switch (number) {
+                case 1:
+                    System.out.println("Number = ONE");
+                    break;
+                case 2:
+                    System.out.println("Number = TWO");
+                    break;
+                case 3:
+                    System.out.println("Number = THREE");
+                    break;
+                case 4:
+                    System.out.println("Number = FOUR");
+                    break;
+                case 5:
+                    System.out.println("Number = FIVE");
+                    break;
+                case 6:
+                    System.out.println("Number = SIX");
+                    break;
+                default:
+                    System.out.println("Cannot give you the name");
+                    run=false;
+            }
         }
+
     }
 }
